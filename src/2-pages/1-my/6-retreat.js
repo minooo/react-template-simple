c8b99b75fdb46730ee080e53b476e2eebe1a7c3c
liushuaibao <1447792802@qq.com>
2018-06-14 17:16:10 +0800

    图片上传成功

--- src/2-pages/1-my/6-retreat.js
+++ src/2-pages/1-my/6-retreat.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import { Toast } from "antd-mobile";
-import { http } from "@utils";
+import { http, wxapi } from "@utils";
 import { Layout, WrapLink } from "@components";
 
 export default class extends Component {
-  state = {};
+  state = {
+    localIds: []
+  };
   componentDidMount() {
     this.onAddress();
   }
@@ -25,12 +27,37 @@ export default class extends Component {
   };
   // 设置
   onSetting = () => {
-    const { reason, order_id } = this.state;
+    const { reason, order_id, localIds } = this.state;
     if (!reason) {
       Toast.info("请填写退货原因", 1);
+    } else if (localIds.length > 0) {
+     wxapi.uploadImages(localIds).then(resolve => {
+      const images = resolve.serverId.join(",");
+      http.postC(
+        {
+          action: "refund",
+          operation: "store",
+          order_id,
+          reason,
+          images
+        },
+        () => {
+          Toast.success("退货原因提交成功", 1, () => {
+            if (window && window.history && window.history.length > 1) {
+              window.history.go(-1);
+            }
+          });
+        }
+      );
+     });
     } else {
       http.postC(
-        { action: "refund", operation: "store", order_id, reason },
+        {
+          action: "refund",
+          operation: "store",
+          order_id,
+          reason
+        },
         () => {
           Toast.success("退货原因提交成功", 1, () => {
             if (window && window.history && window.history.length > 1) {
@@ -41,8 +68,20 @@ export default class extends Component {
       );
     }
   };
+  addPhoto = () => {
+    const { localIds } = wxapi.chooseImage({
+      count: 8,
+      sizeType: "compressed"
+    });
+    console.info(localIds);
+    this.setState(pre => ({ localIds: pre.localIds.concat(localIds) }));
+  };
+  previewImage = item => {
+    const { localIds } = this.state;
+    wxapi.previewImage(item, localIds);
+  };
   render() {
-    const { reason } = this.state;
+    const { reason, localIds } = this.state;
     return (
       <Layout title="申请退货">
         <div className="equal overflow-y">
@@ -54,9 +93,34 @@ export default class extends Component {
             onChange={val => this.onChange(val, "reason")}
           />
           <div className=" h20" />
-          {/* <button onClick={this.onUp} className=" pt30 pl30">
-            <img src={image} alt="" className="feedback-button-bg block" />
-          </button> */}
+          <div className="bg-white plr30 ptb30">
+            {localIds &&
+              localIds.length > 0 &&
+              localIds.map(item => (
+                <div
+                  key={item}
+                  onClick={this.previewImage(item)}
+                  style={{ backgroundImage: `url(${item})` }}
+                />
+              ))}
+            {localIds &&
+              localIds.length < 8 && (
+                <div
+                  className=" bg-borde flex jc-center ai-center"
+                  style={{
+                    width: "1.6rem",
+                    height: "1.6rem",
+                    border: "dashed 2px #d9d9d9"
+                  }}
+                  onClick={this.addPhoto}
+                >
+                  <i
+                    className="i-add"
+                    style={{ color: "#d9d9d9", fontSize: "0.7rem" }}
+                  />
+                </div>
+              )}
+          </div>
           <div className=" h80" />
           <div className="plr30">
             <WrapLink
