0617afc36b92320ac0a67bd4fd44f6ab871fb5f3
minooo <552594996@qq.com>
2018-06-20 18:25:22 +0800

    base

--- src/6-middleware/router.js
+++ src/6-middleware/router.js
@@ -3,7 +3,7 @@ import { common, config, wxapi } from "@utils"
 export default () => next => action => {
   if (action.type === "@@router/LOCATION_CHANGE") {
     if (common.isAndroid()) {
-      wxapi.setConfig({ ...config("wx").jsConfig, debug: true });
+      wxapi.setConfig({ ...config("wx").jsConfig });
     }
     console.info("路由变化了哦！");
   }
